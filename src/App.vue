efa26650eb1717ecf731012d5f725112e5d3ab4c
Alkaidcc <alkaidcc@qq.com>
2023-09-14 15:54:41 +0800

    chore: init project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>{{ mmm?.aaac?.bbb }}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      mmm: {}
+    }
+  },
+}
+</script>
